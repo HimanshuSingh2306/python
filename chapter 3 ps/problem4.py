207b66ce9af5d1b9f0e4044477cdbf504e32ba71
HimanshuSingh2306 <himanshusingh6654321@gmail.com>
2026-05-31 08:51:05 +0530

    a bit chane in comment

--- chapter 3 ps/problem4.py
+++ chapter 3 ps/problem4.py
@@ -1,4 +1,4 @@
 string = "himanshu is a  good boy"
-print(string.find("  "))   #string is immutable always means it can not be changed its original value
+print(string.find("  "))   #strins are immutable always means it can not be changed its original value
 print(string.replace("  "," "))
 print(string)
