6ca9186f9b849c40e2d3bc949af59b5c3fe76319
HimanshuSingh2306 <himanshusingh6654321@gmail.com>
2026-05-31 09:55:07 +0530

    add new problem solution

---
+++ chapter 4/01_list.py
@@ -0,0 +1,7 @@
+friends =["apple", "him", 4 , 4.33 , "rohan", "mohan"]
+#list is mutable means we can change the extisting list but we know string is immutable and we can not chage the existing string got it
+print(friends[0])
+friends[0] = "HImanshu singh"
+print(friends[0])
+#we can store  any type of data type
+#list indexing start from 0
